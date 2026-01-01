603a7a7a045f904f866680cbd527cc58a19df4cc
Karina Tkach <ms.karinatk@gmail.com>
2026-01-01 19:12:14 +0200

    Resolved secrets in tests

--- src/test/java/org/uni/unitravel/UniTravelApplicationTests.java
+++ src/test/java/org/uni/unitravel/UniTravelApplicationTests.java
@@ -2,12 +2,14 @@ package org.uni.unitravel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UniTravelApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
